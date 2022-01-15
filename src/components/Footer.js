9da4a54f606331b236c248c3a91b3ddc411e8799
Nikhil Kumar <nikhil.kumar.18@mountblue.tech>
2022-01-15 11:52:23 +0530

    makes mail msg appear in center

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends Component {
         return (
             <div className='Footer'>
                 <img src={cLogo} className="companylogo" alt='Company Logo'/>
-                <p>Mail us at abc@xyz.com</p>
+                <div className='d-flex flex-row align-items-center'>Mail us at abc@xyz.com</div>
                 <div className='socialmediaicons'>
                     <TiSocialFacebook size={30} title='Facebook' className='footericons'/>
                     <TiSocialTwitter size={30} title='Twitter' className='footericons'/>
